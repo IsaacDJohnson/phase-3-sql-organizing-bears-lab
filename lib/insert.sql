d228fc663881bc2e1d6e63c90ebde6ec457190de
Isaacjohnson <isaac.designws@gmail.com>
2022-09-21 11:59:33 -0500

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 7, 'M', 'Brown', 'Kind', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 41, 'M', 'Black', 'Assertive', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 2, 'F', 'Red', 'Gentle', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 8, 'M', 'White', 'Sweet', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 23, 'F', 'Brown', 'Quirky', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 67, 'M', 'Black', 'Cranky', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 34, 'F', 'Brown', 'Obnoxious', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 12, 'F', 'White', 'Sweet', true);
